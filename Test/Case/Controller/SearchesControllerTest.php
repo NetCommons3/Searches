0019490b59aa5e0bc9726480d908e751b94d300e
s-nakajima <nakajimashouhei@gmail.com>
2016-05-02 11:24:32 +0900

    phpdoc修正

--- Test/Case/Controller/SearchesControllerTest.php
+++ Test/Case/Controller/SearchesControllerTest.php
@@ -4,18 +4,17 @@
  *
  * @author Noriko Arai <arai@nii.ac.jp>
  * @author Shohei Nakajima <nakajimashouhei@gmail.com>
- * @author Noriko Arai <arai@nii.ac.jp>
- * @author Shohei Nakajima <nakajimashouhei@gmail.com>
  * @link http://www.netcommons.org NetCommons Project
  * @license http://www.netcommons.org/license.txt NetCommons License
  * @copyright Copyright 2014, NetCommons Project
-* @copyright Copyright 2014, NetCommons Project
  */
 
 App::uses('SearchesController', 'Searches.Controller');
 
 /**
  * SearchesController Test Case
+ *
+ * @package NetCommons\Searches\Test\Case\Controller
  */
 class SearchesControllerTest extends ControllerTestCase {
 
