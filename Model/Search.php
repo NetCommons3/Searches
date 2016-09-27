426641579650a35ce184b413d4e10c6724d7c9b1
s-nakajima <nakajimashouhei@gmail.com>
2016-09-27 14:35:50 +0900

    typo

--- Model/Search.php
+++ Model/Search.php
@@ -134,7 +134,7 @@ class Search extends Topic {
 
 		//フリーワード
 		if (Hash::get($requests, 'keyword')) {
-			$conditions[] = $this->getStringCondtion(
+			$conditions[] = $this->getStringCondition(
 				$this->alias . '.search_contents',
 				Hash::get($requests, 'keyword'),
 				Hash::get($requests, 'where_type', self::WHERE_TYPE_AND)
@@ -143,7 +143,7 @@ class Search extends Topic {
 
 		//ハンドルの指定
 		if (Hash::get($requests, 'handle')) {
-			$conditions[] = $this->getStringCondtion(
+			$conditions[] = $this->getStringCondition(
 				'TrackableCreator.handlename',
 				Hash::get($requests, 'handle'),
 				Hash::get($requests, 'where_type', self::WHERE_TYPE_AND)
@@ -162,7 +162,7 @@ class Search extends Topic {
  * @param int $whereType 条件タイプ
  * @return array
  */
-	public function getStringCondtion($field, $searchValue, $whereType) {
+	public function getStringCondition($field, $searchValue, $whereType) {
 		if ($whereType === self::WHERE_TYPE_PHRASE) {
 			$values = array($searchValue);
 		} else {
